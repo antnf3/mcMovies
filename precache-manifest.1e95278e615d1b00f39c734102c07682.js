f71f796e0814df23132be55b193554021a294bd2
antnf3 <antnf3@gmail.com>
2019-02-06 18:49:16 +0900

    Updates

---
+++ precache-manifest.1e95278e615d1b00f39c734102c07682.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "26eb5bfa622437c9c893",
+    "url": "/mcMovies/static/js/runtime~main.26eb5bfa.js"
+  },
+  {
+    "revision": "ad0b5841ab08f42f1090",
+    "url": "/mcMovies/static/js/main.ad0b5841.chunk.js"
+  },
+  {
+    "revision": "d120fdf0ddba1865b329",
+    "url": "/mcMovies/static/js/1.d120fdf0.chunk.js"
+  },
+  {
+    "revision": "ad0b5841ab08f42f1090",
+    "url": "/mcMovies/static/css/main.4a4c3cab.chunk.css"
+  },
+  {
+    "revision": "a23a8f338ec3e1542055bce232973430",
+    "url": "/mcMovies/index.html"
+  }
+];
